b03bd649ddbbb4fc805fb2317012526c4af4c12c
Davidmac O. Ekeocha <davidmacolisa@gmail.com>
2023-01-09 21:55:44 +0000

    Initial commit

---
+++ conf_paper/src/conf_robma.R
@@ -0,0 +1,9 @@
+# Conference Paper
+# Pandemic Effects on Nigerian Informal Sector: Evidence from Robust Bayessian Meta-Analysis
+# Objs
+# 1. What is the impact of the pandemic on the informal sector?
+# 2. Are there any improvement post-pandemic?
+# 10 Jan 2023
+
+# Loading Packages
+library(tidyverse)
